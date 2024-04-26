7cbf5e3971aebff166f730f5763a6d8133362a0c
Riteshkc1 <159096111+Riteshkc1@users.noreply.github.com>
2024-04-27 03:59:12 +0545

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var usernameInput = document.getElementById("username");
+  var passwordInput = document.getElementById("password");
+
+  usernameInput.value = "test@gmail.com";
+  passwordInput.value = "123456789";
+});
